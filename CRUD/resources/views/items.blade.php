6ce2b2c1aa6a587082293d9a65cdcff5f4f47541
SKSteeve <steeve1997.ss@gmail.com>
2020-10-15 23:37:51 +0300

    removed old blade code because now it load the data with jq ajax

--- CRUD/resources/views/items.blade.php
+++ CRUD/resources/views/items.blade.php
@@ -5,60 +5,61 @@
 @section('form')
 <div class="mx-auto" style="width: 900px;">
   <form method="POST">
-    @csrf
     <div class="form-row">
       <div class="form-group col-md-4">
         <label for="ID">ID</label>
-        <input type="number" class="form-control" id="ID" name="formdata[id]" value="{{@$student['id']}}">
+        <input type="number" class="form-control" id="ID" name="formdata[id]">
       </div>
       <div class="form-group col-md-4">
         <label for="name">Име</label>
-        <input type="text" class="form-control" id="name" name="formdata[name]" value="{{@$student['name']}}">
+        <input type="text" class="form-control" id="name" name="formdata[name]">
       </div>
       <div class="form-group col-md-4">
         <label for="lastName">Фамилия</label>
-        <input type="text" class="form-control" id="lastName" name="formdata[last_name]"  value="{{@$student['last_name']}}">
+        <input type="text" class="form-control" id="lastName" name="formdata[last_name]">
       </div>
     </div>
 
     <div class="form-row">
         <div class="form-group col-md-4">
           <label for="EGN">ЕГН</label>
-          <input type="number" class="form-control" id="EGN" name="formdata[egn]"  value="{{@$student['egn']}}">
+          <input type="number" class="form-control" id="EGN" name="formdata[egn]">
         </div>
         <div class="form-group col-md-4">
           <label for="email">Имейл</label>
-          <input type="email" class="form-control" id="email" name="formdata[email]"  value="{{@$student['email']}}">
+          <input type="email" class="form-control" id="email" name="formdata[email]">
         </div>
         <div class="form-group col-md-4">
           <label for="city">Град</label>
-          <input type="text" class="form-control" id="city" name="formdata[city]"  value="{{@$student['city']}}">
+          <input type="text" class="form-control" id="city" name="formdata[city]">
         </div>
       </div>
 
       <div class="form-row">
         <div class="form-group col-md-4">
           <p>Пол:</p>
-          <input type="radio" id="male" name="formdata[gender]" @if(@$student['gender'] == 'Мъж') checked @endif value="Мъж">
+          <input type="radio" id="male" name="formdata[gender]" value="Мъж">
           <label for="male">Мъж</label><br />
-          <input type="radio" id="fmale" name="formdata[gender]" @if(@$student['gender'] == 'Жена') checked @endif value="Жена">
+          <input type="radio" id="fmale" name="formdata[gender]" value="Жена">
           <label for="fmale">Жена</label>
         </div>
         <div class="form-group col-md-4">
           <p>Предпочитани спортове:</p>
-          <input type="checkbox" id="football" name="formdata[sport_preff][football]" @if(isset($student['sport_preff']['football'])) checked @endif value="football">
+          <input type="checkbox" id="football" name="formdata[sport_preff][football]" value="football">
           <label for="football">Футбол</label><br />
-          <input type="checkbox" id="voleyball" name="formdata[sport_preff][voleyball]" @if(isset($student['sport_preff']['voleyball'])) checked @endif value="voleyball">
+          <input type="checkbox" id="voleyball" name="formdata[sport_preff][voleyball]" value="voleyball">
           <label for="voleyball">Волейбол</label><br />
-          <input type="checkbox" id="swiming" name="formdata[sport_preff][swiming]" @if(isset($student['sport_preff']['swiming'])) checked @endif value="swiming">
+          <input type="checkbox" id="swiming" name="formdata[sport_preff][swiming]" value="swiming">
           <label for="swiming">Плуване</label>
         </div>
         <div class="form-group col-md-4">
             <label for="subj-matter">Избираем предмет:</label>
             <select name="formdata[subject]" id="subj-matter" class="form-control">
-                @foreach($subject as $key => $val)
-                  <option value="{{$key}}" @if($key == @$student['subject']) selected @endif >{{$val}}</option>
-                @endforeach
+              <option value="0">-</option>
+              <option value="1">Биоинформатика</option>
+              <option value="2">Биохимия</option>
+              <option value="3">Екология</option>
+              <option value="4">Биоинженерство</option>
             </select>
         </div>
       </div>
@@ -66,25 +67,25 @@
       <div class="form-row">
         <div class="form-group col-md-12">
             <label for="short-description">Кратко описание</label><br />
-        <textarea id="short-description" class="form-control" rows="3" placeholder="Опиши се (по желание)" name="formdata[description_text]">{{@$student['description_text']}}</textarea><br />
+        <textarea id="short-description" class="form-control" rows="3" placeholder="Търсене по ключова дума/и от описанието на студент." name="formdata[description_text]"></textarea><br />
         </div>
       </div>
       
       <div class="form-row">
         <div class="form-group col">
-            <a class="btn btn-primary" href="{{url('/items')}}">Ново търсене</a>
-            <button class="btn btn-primary" type="submit" name="search" value="search">Търси</button>
+            <a class="newSearching btn btn-primary" href="{{url('/items')}}">Изчисти формуляра</a>
+            <button id="searchBtn" class="btn btn-primary" type="submit" name="search">Търси</button>
         </div>
 
         <div class="form-group col-md-4">
             <select name="formdata[searchGroup]" id="searchGroup" class="form-control text-white bg-dark">
-                @foreach($searchGroup as $key => $val)
-                  <option value="{{$key}}" @if($key == @$student['searchGroup']) selected @endif >{{$val}}</option>
-                @endforeach
+                <option value="0">Покажи Всички</option>
+                <option value="1">Неизтрити</option>
+                <option value="2">Изтрити</option>
             </select>
         </div>
     </div>
-
+    <input type="hidden" value="{{url('/')}}" id="url" name="url">
   </form>
 </div>
 <br /><br />
@@ -94,8 +95,11 @@
 @section('table')
 <div class="mx-auto" style="width: 1300px;">
 
+  {{-- using both ajax and session to get message for delete and reset --}}
+  <div class="ajaxMessage"></div>
+
   @if(session('message'))
-    <div class="alert alert-{{session()->get('messageStatus')}}" role="alert">{{session()->get('message')}}</div><br />
+    <div class="ajaxMessageSession alert alert-{{session()->get('messageStatus')}}" role="alert">{{session()->get('message')}}</div><br />
   @endif
 
   <table class="table table-hover table-sm table-bordered">
@@ -118,30 +122,12 @@
       </tr>
     </thead>
     <tbody>
-      @foreach($students as $student)
-        <tr>
-          <th>{{$loop->index+1}}</th>
-          <th class="text-danger">{{$student['id']}}</th>
-          <th>{{$student['name']}}</th>
-          <th>{{$student['last_name']}}</th>
-          <th>{{$student['egn']}}</th>
-          <th>{{$student['email']}}</th>
-          <th>{{$student['city']}}</th>
-          <th>{{$student['gender']}}</th>
-          <th>{{$student['sport_preff']}}</th>
-          <th>@if($student['subject'] == 1) {{$subject[1]}} @elseif($student['subject'] == 2) {{$subject[2]}} @elseif($student['subject'] == 3) {{$subject[3]}} @elseif($student['subject'] == 4) {{$subject[4]}}@endif</th>
-          <th>{{$student['description_text']}}</th>
-
-          @if(isset($student['deleted_at']))
-            <th><a href="{{url('restore', $student['id'])}}" class="btn btn-secondary">Възстанови</a></th>
-          @else
-            <th><a href="{{url('item', $student['id'])}}" class="btn btn-primary">Редактирай</a></th>
-          @endif
-
-          <th><a href="{{url('delete', $student['id'])}}" class="btn btn-danger">@if(isset($student['deleted_at'])) <i class="fas fa-trash-alt"></i> @else Изтрий @endif</a></th>
-        </tr>
-      @endforeach
+        {{-- ajax table data here from itemsAjax.js --}}
     </tbody>
   </table>
 </div>
+@endsection
+
+@section('js')
+  <script src="{{ asset('js/itemsAjax.js') }}"></script>
 @endsection
